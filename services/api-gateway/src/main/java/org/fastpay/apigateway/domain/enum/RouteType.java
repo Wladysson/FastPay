15eb7df9058dc0238d51b80746f0a1f8de7ec991
wladysson <wladysonaraujo991@gmail.com>
2026-06-29 02:14:05 -0300

    feat(api-gateway): add route type enumeration, define supported gateway destinations, prepare route resolution layer

---
+++ services/api-gateway/src/main/java/org/fastpay/apigateway/domain/enum/RouteType.java
@@ -0,0 +1,16 @@
+package com.fastpay.apigateway.domain.enums;
+
+// Defines downstream route types
+public enum RouteType {
+
+    MERCHANT,
+
+    PAYMENT,
+
+    TRANSACTION,
+
+    SETTLEMENT,
+
+    NOTIFICATION
+
+}
